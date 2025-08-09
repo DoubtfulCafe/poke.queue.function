f8ea4123c428bc2e0473fd7bb541eb66dd110f26
Daniel Villeda <dvilleda2007@gmail.com>
2025-08-08 21:29:34 -0600

    fix: bugs

--- function_app.py
+++ function_app.py
@@ -1,10 +1,17 @@
 import azure.functions as func
+import datetime
+import json
 import logging
 
 app = func.FunctionApp()
 
-@app.queue_trigger(arg_name="azqueue", queue_name="requests",
-                               connection="sapokequeuedvdev_STORAGE") 
-def requests(azqueue: func.QueueMessage):
-    logging.info('Python Queue trigger processed a message: %s',
-                azqueue.get_body().decode('utf-8'))
+@app.queue_trigger(
+    arg_name="azqueue"
+    , queue_name="requests"
+    , connection="QueueAzureWebJobsStorage"
+)
+def QueueTriggerPokeReport(azqueue: func.QueueMessage):
+    body = azqueue.get_body().decode('utf-8')
+    record = json.loads(body)
+    print(f"Record JSON: {record}")
+    logging.info(f"message: {record}")
